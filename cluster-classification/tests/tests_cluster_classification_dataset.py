14cf542c457309c39f9c82e570fad26992b62f6c
Quentin Schultz <quentin@theschultzfamily.us>
2026-05-26 13:44:53 -0500

    Create testing data for the cluster generator

--- cluster-classification/tests/tests_cluster_classification_dataset.py
+++ cluster-classification/tests/tests_cluster_classification_dataset.py
@@ -116,3 +116,526 @@ class TestClusterClassificationDataset(unittest.TestCase):
             location,
             cluster_classification_dataset.get_hcal_location(card, i_eta, i_phi)
         )
+
+    def data_for__test_data_wrapper__cluster_generator_correctness(feature, event, card, final):
+        """
+        A simple method that serves as a datasource for the cluster_generator.
+        It allows for control over the data provided to the test and improves
+        test duration.
+        """
+
+        # 6 total clusters will be generaed by this process:
+        #   - 4 clusters are in event 0, 2 in event 1
+        #   - Cards 0, 3, 6, 9, 12, 15 are tested
+        #   - All SLR numbers are tested
+        #   - All link numbers are tested
+        #   - Cluster positions 0, 2, 4, 5, 6, 7 are tested
+        #   - All i_phi tower values are tested
+        #   - i_eta tower values 0, 2, 7, 10, 15, 16 are tested
+        #
+        #   - ecal towers 0, 3, 4, 19, 21, 29
+        #   - hcal towers 0, 10, 11, 29, 30, and 'none'
+
+        """ TEST CLUSTER 0 -- SLR0, Link 5, high link
+             0: cluster_et              1
+             1: cluster_seed            2
+             2: cluster_et5x5           3
+             3: cluster_et2x5           4
+             4: cluster_timing          5
+             5: cluster_spike           6
+             6: cluster_brems           7
+             7: cluster_satur           8
+             8: cluster_spare           9
+             9: ecal_tower_et           10
+            10: ecal_tower_timing       11
+            11: ecal_tower_spike        12
+            12: hcal_et                 13
+            13: hcal_fb                 14
+            
+            Event: 0
+            Card: 0
+            SLR: 0
+            Cluster position: 0
+            Link: 5
+            hcal_tower: 0
+            ecal_tower: 3
+            i_eta: 0 (tower)
+            i_phi: 2 (tower)
+            """  
+        if event == 0 and card == 0:
+            match feature:
+                case 'SLR0_cluster_eta':
+                    if final == 0: # cluster position
+                        return 0
+                case 'SLR0_cluster_phi':
+                    if final == 0:
+                        return 10
+                
+                case 'SLR0_cluster_energy':
+                    if final == 0: # cluster position
+                        return 1
+                case 'SLR0_cluster_seed_energy':
+                    if final == 0:
+                        return 2
+                case 'SLR0_cluster_et5x5':
+                    if final == 0:
+                        return 3
+                case 'SLR0_cluster_et2x5':
+                    if final == 0:
+                        return 4
+                case 'SLR0_cluster_timing':
+                    if final == 0:
+                        return 5
+                case 'SLR0_cluster_spike':
+                    if final == 0:
+                        return 6
+                case 'SLR0_cluster_brems':
+                    if final == 0:
+                        return 7
+                case 'SLR0_cluster_satur':
+                    if final == 0:
+                        return 8
+                case 'SLR0_cluster_spare':
+                    if final == 0:
+                        return 9
+                
+                case 'ECALUnclusteredSLR0_tower_et':
+                    if final == 3: # ecal tower
+                        return 10
+                case 'ECALUnclusteredSLR0_tower_timing':
+                    if final == 3:
+                        return 11
+                case 'ECALUnclusteredSLR0_tower_spike':
+                    if final == 3:
+                        return 12
+
+                case 'HCAL5_tower_et':
+                    if final == 0: # hcal tower
+                        return 13
+                case 'HCAL5_tower_fb':
+                    if final == 0:
+                        return 14
+
+                case _:
+                    return 0
+            return 0
+
+        """ TEST CLUSTER 1 -- SLR1, Link 7, high link
+             0: cluster_et              2
+             1: cluster_seed            3
+             2: cluster_et5x5           5
+             3: cluster_et2x5           7
+             4: cluster_timing          11
+             5: cluster_spike           13
+             6: cluster_brems           17
+             7: cluster_satur           23
+             8: cluster_spare           29
+             9: ecal_tower_et           31
+            10: ecal_tower_timing       37
+            11: ecal_tower_spike        41
+            12: hcal_et                 43
+            13: hcal_fb                 47
+            
+            Event: 0
+            Card: 3
+            SLR: 1
+            Cluster position: 2
+            Link: 7
+            hcal_tower: 10
+            ecal_tower: 0
+            i_eta: 2 (tower)
+            i_phi: 0 (tower)
+            """
+        if event == 0 and card == 3:
+            match feature:
+                case 'SLR1_cluster_eta':
+                    if final == 2: # cluster position
+                        return 12
+                case 'SLR1_cluster_phi':
+                    if final == 2:
+                        return 1
+                
+                case 'SLR1_cluster_energy':
+                    if final == 2: # cluster position
+                        return 2
+                case 'SLR1_cluster_seed_energy':
+                    if final == 2:
+                        return 3
+                case 'SLR1_cluster_et5x5':
+                    if final == 2:
+                        return 5
+                case 'SLR1_cluster_et2x5':
+                    if final == 2:
+                        return 7
+                case 'SLR1_cluster_timing':
+                    if final == 2:
+                        return 11
+                case 'SLR1_cluster_spike':
+                    if final == 2:
+                        return 13
+                case 'SLR1_cluster_brems':
+                    if final == 2:
+                        return 17
+                case 'SLR1_cluster_satur':
+                    if final == 2:
+                        return 23
+                case 'SLR1_cluster_spare':
+                    if final == 2:
+                        return 29
+                
+                case 'ECALUnclusteredSLR1_tower_et':
+                    if final == 0: # ecal tower
+                        return 31
+                case 'ECALUnclusteredSLR1_tower_timing':
+                    if final == 0:
+                        return 37
+                case 'ECALUnclusteredSLR1_tower_spike':
+                    if final == 0:
+                        return 41
+
+                case 'HCAL7_tower_et':
+                    if final == 10: # hcal tower
+                        return 43
+                case 'HCAL7_tower_fb':
+                    if final == 10:
+                        return 47
+
+                case _:
+                    return 0
+            return 0
+
+        """ TEST CLUSTER 2 -- SLR2 , Link6, low link
+             0: cluster_et              1
+             1: cluster_seed            1
+             2: cluster_et5x5           2
+             3: cluster_et2x5           3
+             4: cluster_timing          5
+             5: cluster_spike           8
+             6: cluster_brems           13
+             7: cluster_satur           21
+             8: cluster_spare           34
+             9: ecal_tower_et           55
+            10: ecal_tower_timing       -39
+            11: ecal_tower_spike        16
+            12: hcal_et                 -23
+            13: hcal_fb                 -7
+            
+            Event: 0
+            Card: 6
+            SLR: 2
+            Cluster position: 4
+            Link: 6
+            hcal_tower: 11
+            ecal_tower: 21
+            i_eta: 10 (tower)
+            i_phi: 3 (tower)
+            """
+        if event == 0 and card == 6:
+            match feature:
+                case 'SLR2_cluster_eta':
+                    if final == 4: # cluster position
+                        return 54
+                case 'SLR2_cluster_phi':
+                    if final == 4:
+                        return 16
+                
+                case 'SLR2_cluster_energy':
+                    if final == 4: # cluster position
+                        return 1
+                case 'SLR2_cluster_seed_energy':
+                    if final == 4:
+                        return 1
+                case 'SLR2_cluster_et5x5':
+                    if final == 4:
+                        return 2
+                case 'SLR2_cluster_et2x5':
+                    if final == 4:
+                        return 3
+                case 'SLR2_cluster_timing':
+                    if final == 4:
+                        return 5
+                case 'SLR2_cluster_spike':
+                    if final == 4:
+                        return 8
+                case 'SLR2_cluster_brems':
+                    if final == 4:
+                        return 13
+                case 'SLR2_cluster_satur':
+                    if final == 4:
+                        return 21
+                case 'SLR2_cluster_spare':
+                    if final == 4:
+                        return 34
+                
+                case 'ECALUnclusteredSLR2_tower_et':
+                    if final == 21: # ecal tower
+                        return 55
+                case 'ECALUnclusteredSLR2_tower_timing':
+                    if final == 21:
+                        return -39
+                case 'ECALUnclusteredSLR2_tower_spike':
+                    if final == 21:
+                        return 16
+
+                case 'HCAL7_tower_et':
+                    if final == 11: # hcal tower
+                        return -23
+                case 'HCAL7_tower_fb':
+                    if final == 11:
+                        return -7
+
+                case _:
+                    return 0
+            return 0
+
+        """ TEST CLUSTER 3 -- SLR3, Link8, high link
+             0: cluster_et              1
+             1: cluster_seed            8
+             2: cluster_et5x5           2
+             3: cluster_et2x5           9
+             4: cluster_timing          3
+             5: cluster_spike           10
+             6: cluster_brems           4
+             7: cluster_satur           11
+             8: cluster_spare           5
+             9: ecal_tower_et           12
+            10: ecal_tower_timing       6
+            11: ecal_tower_spike        13
+            12: hcal_et                 7
+            13: hcal_fb                 14
+            
+            Event: 0
+            Card: 9
+            SLR: 3
+            Cluster position: 5
+            Link: 8
+            hcal_tower: 29
+            ecal_tower: 19
+            i_eta: 15 (tower)
+            i_phi: 1 (tower)
+            """
+        if event == 0 and card == 9:
+            match feature:
+                case 'SLR3_cluster_eta':
+                    if final == 5: # cluster position
+                        return 76 # trombones
+                case 'SLR3_cluster_phi':
+                    if final == 5:
+                        return 8
+                
+                case 'SLR3_cluster_energy':
+                    if final == 5: # cluster position
+                        return 1
+                case 'SLR3_cluster_seed_energy':
+                    if final == 5:
+                        return 8
+                case 'SLR3_cluster_et5x5':
+                    if final == 5:
+                        return 2
+                case 'SLR3_cluster_et2x5':
+                    if final == 5:
+                        return 9
+                case 'SLR3_cluster_timing':
+                    if final == 5:
+                        return 3
+                case 'SLR3_cluster_spike':
+                    if final == 5:
+                        return 10
+                case 'SLR3_cluster_brems':
+                    if final == 5:
+                        return 4
+                case 'SLR3_cluster_satur':
+                    if final == 5:
+                        return 11
+                case 'SLR3_cluster_spare':
+                    if final == 5:
+                        return 5
+                
+                case 'ECALUnclusteredSLR3_tower_et':
+                    if final == 19: # ecal tower
+                        return 12
+                case 'ECALUnclusteredSLR3_tower_timing':
+                    if final == 19:
+                        return 6
+                case 'ECALUnclusteredSLR3_tower_spike':
+                    if final == 19:
+                        return 13
+
+                case 'HCAL8_tower_et':
+                    if final == 29: # hcal tower
+                        return 7
+                case 'HCAL8_tower_fb':
+                    if final == 29:
+                        return 14
+
+                case _:
+                    return 0
+            return 0
+        
+        """ TEST CLUSTER 4 -- SLR2, Link 5, high link
+             0: cluster_et              14
+             1: cluster_seed            13
+             2: cluster_et5x5           12
+             3: cluster_et2x5           11
+             4: cluster_timing          10
+             5: cluster_spike           9
+             6: cluster_brems           8
+             7: cluster_satur           7
+             8: cluster_spare           6
+             9: ecal_tower_et           5
+            10: ecal_tower_timing       4
+            11: ecal_tower_spike        3
+            12: hcal_et                 2
+            13: hcal_fb                 1
+            
+            Event: 1
+            Card: 12
+            SLR: 2
+            Cluster position: 6
+            Link: 5
+            hcal_tower: 30
+            ecal_tower: 4
+            i_eta: 7 (tower)
+            i_phi: 4 (tower)
+            """
+        if event == 1 and card == 12:
+            match feature:
+                case 'SLR2_cluster_eta':
+                    if final == 6: # cluster position
+                        return 37
+                case 'SLR2_cluster_phi':
+                    if final == 6:
+                        return 23
+                
+                case 'SLR2_cluster_energy':
+                    if final == 6: # cluster position
+                        return 14
+                case 'SLR2_cluster_seed_energy':
+                    if final == 6:
+                        return 13
+                case 'SLR2_cluster_et5x5':
+                    if final == 6:
+                        return 12
+                case 'SLR2_cluster_et2x5':
+                    if final == 6:
+                        return 11
+                case 'SLR2_cluster_timing':
+                    if final == 6:
+                        return 10
+                case 'SLR2_cluster_spike':
+                    if final == 6:
+                        return 9
+                case 'SLR2_cluster_brems':
+                    if final == 6:
+                        return 8
+                case 'SLR2_cluster_satur':
+                    if final == 6:
+                        return 7
+                case 'SLR2_cluster_spare':
+                    if final == 6:
+                        return 6
+                
+                case 'ECALUnclusteredSLR2_tower_et':
+                    if final == 4: # ecal tower
+                        return 5
+                case 'ECALUnclusteredSLR2_tower_timing':
+                    if final == 4:
+                        return 4
+                case 'ECALUnclusteredSLR2_tower_spike':
+                    if final == 4:
+                        return 3
+
+                case 'HCAL5_tower_et':
+                    if final == 30: # hcal tower
+                        return 2
+                case 'HCAL5_tower_fb':
+                    if final == 30:
+                        return 1
+
+                case _:
+                    return 0
+            return 0
+
+        """ TEST CLUSTER 5 -- SLR3, no link
+             0: cluster_et              11
+             1: cluster_seed            9
+             2: cluster_et5x5           7
+             3: cluster_et2x5           5
+             4: cluster_timing          3
+             5: cluster_spike           1
+             6: cluster_brems           0
+             7: cluster_satur           2
+             8: cluster_spare           4
+             9: ecal_tower_et           6
+            10: ecal_tower_timing       8
+            11: ecal_tower_spike        10
+            12: hcal_et                 -1
+            13: hcal_fb                 -1
+            
+            Event: 1
+            Card: 15
+            SLR: 3
+            Cluster position: 7
+            Link: N/A
+            hcal_tower: N/A
+            ecal_tower: 29
+            i_eta: 16 (tower)
+            i_phi: 5 (tower)
+            """
+        if event == 1 and card == 15:
+            match feature:
+                case 'SLR3_cluster_eta':
+                    if final == 7: # cluster position
+                        return 84
+                case 'SLR3_cluster_phi':
+                    if final == 7:
+                        return 29
+                
+                case 'SLR3_cluster_energy':
+                    if final == 7: # cluster position
+                        return 11
+                case 'SLR3_cluster_seed_energy':
+                    if final == 7:
+                        return 9
+                case 'SLR3_cluster_et5x5':
+                    if final == 7:
+                        return 7
+                case 'SLR3_cluster_et2x5':
+                    if final == 7:
+                        return 5
+                case 'SLR3_cluster_timing':
+                    if final == 7:
+                        return 3
+                case 'SLR3_cluster_spike':
+                    if final == 7:
+                        return 1
+                case 'SLR3_cluster_brems':
+                    if final == 7:
+                        return 0
+                case 'SLR3_cluster_satur':
+                    if final == 7:
+                        return 2
+                case 'SLR3_cluster_spare':
+                    if final == 7:
+                        return 4
+                
+                case 'ECALUnclusteredSLR3_tower_et':
+                    if final == 29: # ecal tower
+                        return 6
+                case 'ECALUnclusteredSLR3_tower_timing':
+                    if final == 29:
+                        return 8
+                case 'ECALUnclusteredSLR3_tower_spike':
+                    if final == 29:
+                        return 10
+
+                case _:
+                    return 0
+            return 0
+
+        return 0
+
+    def test_data_wrapper__cluster_generator_correctness(self):
+        
+
+        pass
