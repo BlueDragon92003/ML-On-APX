482cec6ee19d0c8d2079daaf1b983024bca6dd57
Quentin Schultz <quentin@theschultzfamily.us>
2026-08-20 16:20:25 -0500

    Project maintenance

--- ml_on_apx/__init__.py
+++ ml_on_apx/__init__.py
@@ -1,1 +1,4 @@
 """Cluster Analysis ML Model for CMS L1."""
+
+# TODO Separate textual event handlers with @on(MESSAGE, "tcss selector")
+# TODO Make sure all custom messages have @property control defined.
